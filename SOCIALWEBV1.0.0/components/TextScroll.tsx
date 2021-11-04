05b30d627ebdd6ccd7f34584c9d7e275b565eede
Swarnendu Chatterjee <swarnendubabai32@gmail.com>
2021-11-04 14:02:33 -0700

    commited

--- SOCIALWEBV1.0.0/components/TextScroll.tsx
+++ SOCIALWEBV1.0.0/components/TextScroll.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { TextScrollProps } from "../utility/types/other_types";
 const TextScroll = ({ children }: TextScrollProps) => {
-    // This is a wrapper component the wraps
+    // This is a wrapper component.
     return (
         <SafeAreaView style={styles.scroller}>
             <ScrollView>{children}</ScrollView>
