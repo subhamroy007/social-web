f1696dfbb5dd2ae9a02477f2298553d070217688
subhamroy007 <subhamroy199907@gmail.com>
2021-11-07 21:11:43 +0530

    collapasable component and resource container is updatd

--- SOCIALWEBV1.0.0/components/CollapsibleText.tsx
+++ SOCIALWEBV1.0.0/components/CollapsibleText.tsx
@@ -2,75 +2,75 @@ import { StyleSheet, Text } from "react-native";
 import React, { useCallback, useMemo, useState } from "react";
 import { CollapseTextProps } from "../utility/types/other_types";
 
-import { THERESHOLDLENGTH } from "../utility/constants/AppConstants";
+import { THERESHOLDLENGTH } from "../utility/constants/appConstants";
 
 const CollapsibleText = ({ content }: CollapseTextProps) => {
-    const length = content.length;
-    const text = content;
-    // isCollapsed is react state variable maintained to check whether the text component is expanded or not. The deafult value is true.
-    const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
+  const length = content.length;
+  const text = content;
+  // isCollapsed is react state variable maintained to check whether the text component is expanded or not. The deafult value is true.
+  const [isCollapsed, setIsCollapsed] = useState<boolean>(true);
 
-    // newLinePosition state variable is used to store the the position of '\n' in the text string. The default value is 0.
-    const [newLinePosition, setNewLinePosition] = useState<number>(0);
+  // newLinePosition state variable is used to store the the position of '\n' in the text string. The default value is 0.
+  const [newLinePosition, setNewLinePosition] = useState<number>(0);
 
-    // textSliceHandler function returns a substring from the original string based on the position of the first occurance of '\n', in comparison with THRESHOLD length.
-    const textSliceHandler = useMemo((): string => {
-        let slice: string = ""; // Variable to store the sliced substring, initialised with an empty string
-        let position: number = text.indexOf("\n"); // Stores the position of the first occurance of '\n' in the original string
-        setNewLinePosition(position);
-        if (position > THERESHOLDLENGTH - 1) {
-            slice = text.slice(0, THERESHOLDLENGTH + 1);
-        } else {
-            slice = text.slice(0, position);
-        }
-        return slice;
-    }, []);
+  // textSliceHandler function returns a substring from the original string based on the position of the first occurance of '\n', in comparison with THRESHOLD length.
+  const textSliceHandler = useMemo((): string => {
+    let slice: string = ""; // Variable to store the sliced substring, initialised with an empty string
+    let position: number = text.indexOf("\n"); // Stores the position of the first occurance of '\n' in the original string
+    setNewLinePosition(position);
+    if (position > THERESHOLDLENGTH - 1) {
+      slice = text.slice(0, THERESHOLDLENGTH + 1);
+    } else {
+      slice = text.slice(0, position);
+    }
+    return slice;
+  }, []);
 
-    const resultText: string = isCollapsed ? textSliceHandler : text; // Contains the resultant string based the isCollapsed state.
+  const resultText: string = isCollapsed ? textSliceHandler : text; // Contains the resultant string based the isCollapsed state.
 
-    // A callback function to control the value of the isCollapsed state.
-    const collapseStateHandler = useCallback(() => {
-        if (isCollapsed) {
-            setIsCollapsed(false);
-        } else {
-            setIsCollapsed(true);
-        }
-    }, [isCollapsed]);
+  // A callback function to control the value of the isCollapsed state.
+  const collapseStateHandler = useCallback(() => {
+    if (isCollapsed) {
+      setIsCollapsed(false);
+    } else {
+      setIsCollapsed(true);
+    }
+  }, [isCollapsed]);
 
-    // Return React components conditionally to expand or collapse the text.
-    const collapseToggler = isCollapsed ? (
-        <Text onPress={collapseStateHandler} style={styles.collapseToggler}>
-            {" "}
-            Read more
-        </Text>
-    ) : (
-        <Text onPress={collapseStateHandler} style={styles.collapseToggler}>
-            {" "}
-            Read less
-        </Text>
-    );
+  // Return React components conditionally to expand or collapse the text.
+  const collapseToggler = isCollapsed ? (
+    <Text onPress={collapseStateHandler} style={styles.collapseToggler}>
+      {" "}
+      Read more
+    </Text>
+  ) : (
+    <Text onPress={collapseStateHandler} style={styles.collapseToggler}>
+      {" "}
+      Read less
+    </Text>
+  );
 
-    return (
-        <Text style={styles.collapse}>
-            {resultText}
-            {newLinePosition < THERESHOLDLENGTH
-                ? collapseToggler
-                : length < THERESHOLDLENGTH
-                ? null
-                : collapseToggler}
-        </Text>
-    );
+  return (
+    <Text style={styles.collapse}>
+      {resultText}
+      {newLinePosition < THERESHOLDLENGTH
+        ? collapseToggler
+        : length < THERESHOLDLENGTH
+        ? null
+        : collapseToggler}
+    </Text>
+  );
 };
 
 export default CollapsibleText;
 
 const styles = StyleSheet.create({
-    collapse: {
-        width: "100%",
-        fontSize: 14,
-    },
-    collapseToggler: {
-        color: "gray",
-        fontSize: 16,
-    },
+  collapse: {
+    width: "100%",
+    fontSize: 14,
+  },
+  collapseToggler: {
+    color: "gray",
+    fontSize: 16,
+  },
 });
--- SOCIALWEBV1.0.0/components/ResourceContainer.tsx
+++ SOCIALWEBV1.0.0/components/ResourceContainer.tsx
@@ -4,42 +4,42 @@ import { MaterialIcons } from "@expo/vector-icons";
 import { ResourceContainnerProps } from "../utility/types/other_types";
 
 const ResourceContainer = ({
-    iconName,
-    title,
-    url,
+  iconName,
+  title,
+  url,
 }: ResourceContainnerProps) => {
-    return (
-        <Pressable style={styles.touchable} onPress={useCallback(() => {}, [])}>
-            <View style={styles.container}>
-                <MaterialIcons
-                    style={styles.icon}
-                    name="email"
-                    size={20}
-                    color="black"
-                />
-                <Text style={styles.text}>{title}</Text>
-            </View>
-        </Pressable>
-    );
+  return (
+    <Pressable style={styles.touchable} onPress={useCallback(() => {}, [])}>
+      <View style={styles.container}>
+        <MaterialIcons
+          style={styles.icon}
+          name="email"
+          size={20}
+          color="black"
+        />
+        <Text style={styles.text}>{title}</Text>
+      </View>
+    </Pressable>
+  );
 };
 
 export default ResourceContainer;
 
 const styles = StyleSheet.create({
-    touchable: {
-        backgroundColor: "#EBE8FB",
-        borderRadius: 30,
-    },
-    container: {
-        flexDirection: "row",
-        justifyContent: "center",
-        margin: 5,
-    },
-    icon: {
-        marginHorizontal: 4,
-    },
-    text: {
-        marginHorizontal: 4,
-        fontWeight: "bold",
-    },
+  touchable: {
+    backgroundColor: "#EBE8FB",
+    borderRadius: 30,
+  },
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    margin: 5,
+  },
+  icon: {
+    marginHorizontal: 4,
+  },
+  text: {
+    marginHorizontal: 4,
+    fontWeight: "bold",
+  },
 });
