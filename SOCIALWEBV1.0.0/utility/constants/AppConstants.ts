38a8f100fec635053d2a8e022af0b2af589afec3
subhamroy007 <subhamroy199907@gmail.com>
2021-11-07 21:54:44 +0530

    error resolved on constatnts file

--- SOCIALWEBV1.0.0/utility/constants/AppConstants.ts
+++ SOCIALWEBV1.0.0/utility/constants/AppConstants.ts
@@ -1,1 +1,50 @@
-export const THERESHOLDLENGTH: number = 15;
+import { Dimensions } from "react-native";
+
+export const WINDOW_WIDTH = Dimensions.get("window").width;
+export const WINDOW_HEIGHT = Dimensions.get("window").height;
+
+export const IMAGE_POST_MIN_HEIGHT = 200;
+export const IMAGE_POST_MAX_HEIGHT = 500;
+export const SHUTTER_HEIGHT = Math.floor(WINDOW_HEIGHT * 0.47);
+export const SHUTTER_ABSOLUTE_TOP_POSITION =
+  WINDOW_HEIGHT - Math.floor((SHUTTER_HEIGHT * 4) / 25);
+export const SHUTTER_TRANSLATION_Y_MIN =
+  Math.floor((SHUTTER_HEIGHT * 4) / 25) - SHUTTER_HEIGHT;
+export const SHUTTER_TRANSLATION_Y_MAX = 0;
+
+export const AVATAR_PHOTO_TO_GAP_RATIO = 2.5 / 64;
+
+export const SHUTTER_BODY_LIST_ITEM_HEIGHT =
+  SHUTTER_HEIGHT * (18 / 25) * (4 / 5);
+
+export const SHUTTER_ICON_SIZE = Math.floor(
+  SHUTTER_BODY_LIST_ITEM_HEIGHT * 0.27
+);
+
+export const ROUNDED_ICON_SIZE_TO_GAP_RATIO = 3 / 24;
+
+export const SHUTTER_ICON_VERTICAL_MARGIN =
+  (SHUTTER_BODY_LIST_ITEM_HEIGHT -
+    2 *
+      (SHUTTER_ICON_SIZE +
+        2 * SHUTTER_ICON_SIZE * ROUNDED_ICON_SIZE_TO_GAP_RATIO)) /
+  2;
+
+export const SHUTTER_ICON_HORIZONTAL_MARGIN =
+  (WINDOW_WIDTH -
+    2 *
+      (SHUTTER_ICON_SIZE +
+        2 * SHUTTER_ICON_SIZE * ROUNDED_ICON_SIZE_TO_GAP_RATIO)) /
+  7.5;
+
+export const TAG_IMAGE_SIZE_RATIO = 16 / 320;
+
+export const TAG_TEXT_SIZE_RATIO = 10 / 320;
+
+export const TAG_GAP_RAIO = 4 / 320;
+
+export const TAG_BORDER_RADIUS_RATIO = 16 / 320;
+
+export const USER_INFO_PRIMARY_TEXT_SIZE = (12 / 320) * WINDOW_WIDTH;
+
+export const USER_INFO_SECONDARY_TEXT_SIZE = (10 / 320) * WINDOW_WIDTH;
